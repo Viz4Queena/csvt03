992805992f8d3aa5e863b0927f552555373bb2d1
viking <root@localhost.localdomain>
2014-07-30 04:42:17 -0700

    csvt03

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from blog.models import Employee
+from django.shortcuts import render_to_response
+
+
+def index(req):
+   emps = Employee.objects.all()
+   return render_to_response('index.html',{'emps':emps})
